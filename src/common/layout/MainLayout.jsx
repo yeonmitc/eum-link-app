fe487714b1039249947f94438c003778e5929070
jin <jin200126@gmail.com>
2025-04-30 21:05:21 +0900

    add icon site

--- src/common/layout/MainLayout.jsx
+++ src/common/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import FixedBtn from '../components/FixedBtn';
 
 export default function MainLayout() {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Header />
+      <FixedBtn />
       <main className="flex w-full flex-1 flex-col items-center justify-center">
         <Outlet />
       </main>
