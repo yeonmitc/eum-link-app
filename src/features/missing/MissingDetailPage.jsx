879d6e50e8da6a6690db67a1a4da22462637fb8f
roseraph502on <sera6768@naver.com>
2025-04-30 19:06:43 +0900

    Update MissingDetailPage.jsx
    init

--- src/features/missing/MissingDetailPage.jsx
+++ src/features/missing/MissingDetailPage.jsx
@@ -1,5 +1,16 @@
-const MissingDetailPage = () => {
-  return <div>MissingDetailPage</div>;
-};
+import React from 'react'
 
-export default MissingDetailPage;
+const MissingDetailPage = () => {
+  return (
+<div class="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+<a className="inline-block rounded-sm border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:ring-3 focus:outline-hidden"
+  href="#"
+>이름 | 상세종</a>
+</div>
+
+
+ 
+  )
+}
+
+export default MissingDetailPage
