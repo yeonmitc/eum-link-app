46172de2c0ce00d24e259b68c77edc98e3e574eb
roseraph502on <sera6768@naver.com>
2025-05-01 01:07:08 +0900

    feat : #10 실종 상세 ui

--- src/features/missing/MissingDetailPage.jsx
+++ src/features/missing/MissingDetailPage.jsx
@@ -14,15 +14,15 @@ const MissingDetailPage = () => {
       </Grid>
 
       <Grid container size={12} >
-        <Box sx={{width:'96vw',height: '72vh',background:' #F5EEDD',textAlign:'center',borderRadius:'0 20px 20px 20px', padding:'2% 3%'}}>
+        <Box sx={{width:'96vw',height: '72vh',background:' #F5EEDD',textAlign:'center',borderRadius:'0 20px 20px 20px', padding: '4vh 5vw'}}>
           {/* 정보 */}
-          <Grid size={12}  sx={{width:'92vw',height: '34vh', display:'flex'}} >
+          <Grid size={12}  sx={{width:'86vw',height: '34vh', display:'flex'}} >
               <Grid size={4}>
-                <Card sx={{ maxWidth: '22vw' ,maxHeight: '14vw'}}>
+                <Card sx={{ maxWidth: '24vw' ,maxHeight: '15vw'}}>
                   <img src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"/>
                 </Card>
               </Grid>
-              <Grid size={4} container  spacing={2} sx={{ maxWidth: '20vw' ,maxHeight: '14vw'}} >
+              <Grid size={4} container  spacing={1} sx={{ maxWidth: '20vw' ,maxHeight: '14vw'}} >
                 <Grid container size={12} spacing={0} >이름</Grid>
                 <Grid container size={12} spacing={0} >종</Grid>
                 <Grid container size={12} spacing={0} >성별</Grid>
@@ -30,26 +30,26 @@ const MissingDetailPage = () => {
                 <Grid container size={12} spacing={0} >특징</Grid>
               </Grid>
               <Grid size={4}>
-              <Card sx={{ maxWidth: '20vw' ,maxHeight: '14vw'}}></Card>
+              <Card sx={{ maxWidth: '20vw' ,maxHeight: '15vw'}}></Card>
               </Grid>
           </Grid>
           {/* 댓글 */}
-          <Grid size={12}  sx={{width:'92vw',height: '34vh'}}>
-            <Box sx={{width:'92vw',height: '30vh', background:' #fff',borderRadius:'20px'}} >
-              <Grid size={12} height={'5vh'} lineHeight={'5vh'}>댓글</Grid>
+          <Grid size={12}  sx={{width:'86vw',height: '32vh', background:' #fff',borderRadius:'20px',}}>
+              <Grid size={12} height={'5vh'} lineHeight={'5vh'} display={'flex'} justifyContent={'flex-start'} padding={'1vh 2vw'} color={'gray'} >댓글()</Grid>
               <Grid size={12}height={'20vh'}></Grid>
-              <Grid size={12} height={'5vh'} display={'flex'}>
+              <Grid size={12} height={'5vh'} display={'flex'} padding={'0 10px'}>
                 <InputBase
-                  sx={{ width:'60vw', flex: 1, background:'rgb(228, 228, 228)', paddingLeft: 2, borderRadius:'10px'}}
+                  sx={{ width:'60vw', flex: 1, background:'rgb(228, 228, 228)', paddingLeft: 2, borderRadius:'10px' , marginRight:'2vw'}}
                   placeholder="댓글을 입력하세요."
                   inputProps={{ 'aria-label': '' }}
                 />
-                <Box sx={{ width:'5vh',height:'5vh', background:'rgb(228, 228, 228)'}}></Box>
+                <Box sx={{ width:'5vh',height:'5vh', background:'rgb(228, 228, 228)', borderRadius:'10px'}}></Box>
               </Grid>
-            </Box>
           </Grid>
+
         </Box>
       </Grid>
+      {/* 하단 버튼 */}
       <Grid size={12}sx={{ marginTop:'2vh', display:'flex' ,color:"#fff"}}>
         <Box sx={{width:'96vw',height: '6vh',background:' #436850',textAlign:'center' , lineHeight:'6vh',borderRadius:'20px'}}>
           
