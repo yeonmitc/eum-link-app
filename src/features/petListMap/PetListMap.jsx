ab90035c2fd3799c2a23ef2b892195562347f176
Jaeework <jaeework@gmail.com>
2025-05-02 23:53:11 +0900

    feat:#52 목록/지도 전환 토글 버튼 구현
    - 마우스 호버 시 토글 버튼 생기도록 구현
    - 버튼 클릭 시 목록/지도 뷰 전환

--- src/features/petListMap/PetListMap.jsx
+++ src/features/petListMap/PetListMap.jsx
@@ -1,4 +1,6 @@
 import { usePetListQuery } from '@/hooks/usePetList';
+import { LayoutList, Map } from 'lucide-react';
+import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import ListView from './components/List/ListView';
 import MapView from './components/Map/MapView';
@@ -8,7 +10,8 @@ import TabMenu from './components/TabAndFilter/TabMenu';
 const PetListMap = ({type}) => {
   // url : /missing,report?map=true
   const [searchParams, setSearchParams] = useSearchParams();
-  const isMapView = searchParams.get('map') === true; // 기본값은 false
+  const [isHovered, setIsHovered] = useState(false);
+  const isMapView = searchParams.get('map') === "true"; // 기본값은 false
 
   const {
     data: petList, 
@@ -18,6 +21,10 @@ const PetListMap = ({type}) => {
   } = usePetListQuery({ type });
 
   console.log('petList', petList);
+
+  const toggleViewMap = () => {
+    setSearchParams({map: isMapView ? "false" : "true"})
+  }
 
   if(isPetListLoading) return <div>Loading...</div>
   if(isPetListError) return <div>Error: {petError.message}</div>
@@ -29,7 +36,24 @@ const PetListMap = ({type}) => {
       </div>
       <div className="w-full md:basis-3/4">
         <div className="hidden md:!block"><DesktopFilter /></div>
-        <div>
+
+        <div
+          className="relative"
+          onMouseEnter={()=>setIsHovered(true)}
+          onMouseLeave={()=>setIsHovered(false)}          
+        >
+          {
+            isHovered &&
+            <button 
+              className="flex z-2 justify-center items-center absolute bottom-[3rem] right-1/2 rounded-sm bg-(--bg) text-black shadow-lg gap-x-2 p-2 w-[150px] h-[50px] cursor-pointer"
+              onClick={toggleViewMap}
+            >
+              {isMapView
+              ? <>목록 보기 <LayoutList /></>
+              : <>지도로 보기<Map /></>}
+            </button>
+          }
+
           { isMapView 
           ? <MapView /> 
           : <ListView pets={petList} type={type} />
