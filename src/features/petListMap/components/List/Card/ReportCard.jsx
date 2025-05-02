8f2d08ae2c20c566ad8a3a8be46ea1ebaedfdc5e
Jaeework <jaeework@gmail.com>
2025-05-02 18:22:24 +0900

    feat: #20 목록 페이지 및 카드 레이아웃 구현
    - 목록 페이지 및 카드 레이아웃 구현
    - 탭 레이아웃 구현 및 목록 전환 구현
    - 카드 컴포넌트 상세 페이지 연결 구현
    - 목록 페이지 라우팅 변경

--- src/features/petListMap/components/List/Card/ReportCard.jsx
+++ src/features/petListMap/components/List/Card/ReportCard.jsx
@@ -1,8 +1,10 @@
 import { Cat, Dog, PawPrint } from 'lucide-react';
 
-const MissingCard = ({ pet, speciesList }) => {
+const ReportCard = ({ pet, speciesList }) => {
+
   const species = speciesList?.find(item => item.id === pet.refSpecies);
-  
+  const petBreed = speciesList?.find(item => item.id === parseInt(pet?.subSpecies));
+
   return (
     <div className="rounded-lg shadow-xl h-[350px] md:h-[330px]">
       <div 
@@ -14,31 +16,31 @@ const MissingCard = ({ pet, speciesList }) => {
         }}
         ></div>
       <div className="my-2 ml-2 gap-2 flex items-center">
-        <div className="flex items-center gap-2 px-3 py-1 rounded-full bg-[#5d9471] text-white text-md">
+        <div className="flex items-center gap-2 px-3 py-1 rounded-full bg-[#5d9471] text-gray-100 text-md">
           {pet?.refSpecies === 1 ? <Dog className="w-5 h-5" />
           : pet?.refSpecies === 2 ? <Cat className="w-5 h-5" />
           : <PawPrint className="w-5 h-5" />
           }
           <p>{species?.name || "기타"}</p>
         </div>
-        <div className="text-md font-semibold text-black">{pet?.petName || "미상"}</div>
+        <div className="text-md font-semibold text-black">{petBreed ? petBreed.name : "기타"}</div>
       </div>
       <div className="mx-2 mb-4">
         <div className="mt-2 flex justify-start items-center gap-2">
-          <div className="text-sm text-gray-700">성별</div>
-          <div className="text-sm text-gray-800">{(pet?.petGender === "f" ? "암컷" : "수컷") || "미상" }</div>
+          <div className="text-sm text-gray-700">목격날짜</div>
+          <div className="text-sm text-gray-800">{pet?.sightedAt && new Date(pet.sightedAt).toLocaleDateString()}</div>
         </div>
         <div className="mt-2 flex justify-start items-center gap-2">
-          <div className="text-sm text-gray-700">실종날짜</div>
-          <div className="text-sm text-gray-800">{pet?.lostDate && new Date(pet.lostDate).toLocaleDateString()}</div>
+          <div className="text-sm text-gray-700">목격시간</div>
+          <div className="text-sm text-gray-800">{pet?.sightedAt && new Date(pet.sightedAt).toLocaleTimeString([],{hour:"numeric", minute:"numeric"})}</div>
         </div>
         <div className="mt-2 flex justify-start items-center gap-2">
-          <div className="shrink-0 text-sm text-gray-700">실종장소</div>
-          <div className="truncate text-sm text-gray-800">{pet?.lostLocation.road_address || pet?.lostLocation.number_address}</div>
+          <div className="shrink-0 text-sm text-gray-700">목격장소</div>
+          <div className="truncate text-sm text-gray-800">{pet?.sightedLocation.road_address || pet?.sightedLocation.number_address}</div>
         </div>
       </div>
     </div>
   )
 }
 
-export default MissingCard
+export default ReportCard
