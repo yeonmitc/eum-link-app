27d40c0062894f3b5e28f9d472f102ca8af5b121
Jaeework <jaeework@gmail.com>
2025-05-04 09:31:38 +0900

    feat: #64 이미지 없는 경우, 이미지 처리 구현
    - 이미지 경로 없는 경우를 대비하여, 기본 이미지 주소 설정

--- src/features/petListMap/components/List/Card/MissingCard.jsx
+++ src/features/petListMap/components/List/Card/MissingCard.jsx
@@ -1,23 +1,25 @@
 import { Cat, Dog, PawPrint } from 'lucide-react';
+import basicImage from '../../../../../assets/images/eum-logo.webp';
 
 const MissingCard = ({ pet, speciesList }) => {
   const species = speciesList?.find(item => item.id === pet.refSpecies.toString());
-  
+  const imageUrl = pet?.imageUrl || basicImage;
+
   return (
     <div className="rounded-lg shadow-xl h-[350px] md:h-[330px] overflow-hidden">
-      <div 
+      <div
         className="rounded-lg h-[60%]"
         style={{
-          backgroundImage: `url(${pet?.imageUrl})`,
+          backgroundImage: `url(${imageUrl})`,
           backgroundPosition: "center 40%",
           backgroundSize: "cover"
         }}
-        ></div>
+      ></div>
       <div className="my-2 ml-2 gap-2 flex items-center">
         <div className="flex items-center gap-2 px-3 py-1 rounded-full bg-(--secondary) text-white text-md">
           {pet?.refSpecies === 1 ? <Dog className="w-5 h-5" />
-          : pet?.refSpecies === 2 ? <Cat className="w-5 h-5" />
-          : <PawPrint className="w-5 h-5" />
+            : pet?.refSpecies === 2 ? <Cat className="w-5 h-5" />
+              : <PawPrint className="w-5 h-5" />
           }
           <span className="text-sm truncate">{species?.name || "기타"}</span>
         </div>
@@ -26,7 +28,7 @@ const MissingCard = ({ pet, speciesList }) => {
       <div className="mx-2 mb-4">
         <div className="mt-2 flex justify-start items-center gap-2">
           <div className="text-sm text-gray-700">성별</div>
-          <div className="text-sm text-gray-800">{(pet?.petGender === "f" ? "암컷" : "수컷") || "미상" }</div>
+          <div className="text-sm text-gray-800">{(pet?.petGender === "f" ? "암컷" : "수컷") || "미상"}</div>
         </div>
         <div className="mt-2 flex justify-start items-center gap-2">
           <div className="text-sm text-gray-700">실종날짜</div>
